08d2c7df1e733d21bad62dc864e3ec25c703b671
Nino <nino.arcelin@gmail.com>
2024-12-11 17:54:08 +0100

    [ADD] dashboard and components

--- Tic Tac Toe/src/views/DashboardView.vue
+++ Tic Tac Toe/src/views/DashboardView.vue
@@ -1,10 +1,38 @@
 <script>
+
+import GamesListComponent from '../components/dashboard/GamesListComponent.vue';
+
+
+export default {
+
+  data() {
+    return {
+      games: [
+        {
+          playerName: "Alice",
+          date: "2024-12-10"
+        },
+        {
+          playerName: "Bob",
+          date: "2024-12-11"
+        },
+        {
+          playerName: "Charlie",
+          date: "2024-12-12"
+        }
+      ],
+    }
+  },
+  component: {
+    GamesListComponent,
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <h1>Dashboard</h1>
-    <p>Welcome to the game</p>
-    <p>Start Game</p>
-  </div>
+  <h1>Dashboard</h1>
+  <p>Waiting Games : </p>
+  <GameListComponent :games="games" />
+
 </template>
