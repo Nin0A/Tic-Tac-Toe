cab8716f4c4df6c4914b08d53be8b24703bd31ad
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2025-01-09 15:14:34 +0100

    [ADD] implement registration component and integrate with DataProvider

--- Tic Tac Toe/src/services/DataProvider.js
+++ Tic Tac Toe/src/services/DataProvider.js
@@ -2,9 +2,9 @@ import { request } from "./httpClient";
 
 const login = (username, password) => {
     return request('/login', 'POST', { username, password }, true);
-   };
+};
 const register = (username, password) => {
-return request('/register', 'POST', { username, password }, true);
+    return request('/register', 'POST', { username, password }, true);
 };
 
 export { login, register };
--- Tic Tac Toe/src/views/RegisterView.vue
+++ Tic Tac Toe/src/views/RegisterView.vue
@@ -1,10 +1,14 @@
 <script>
+import RegisterComponent from '@/components/register/RegisterComponent.vue';
+
+export default {
+  components: {
+    RegisterComponent,
+  },
+};
 </script>
 <template>
   <div>
-    <h1>Register</h1>
-    <p>Enter your name to start the game</p>
-    <input type="text" v-model="name" placeholder="Enter your name">
-    <router-link to="/dashboard">Start Game</router-link>
+    <RegisterComponent />
   </div>
 </template>
