3c417157b8496b770a807c09f7854a4033c5122d
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2025-01-09 15:23:38 +0100

    [ADD] update HomeView to include Register link and remove console log from LoginView

--- Tic Tac Toe/src/views/HomeView.vue
+++ Tic Tac Toe/src/views/HomeView.vue
@@ -5,6 +5,7 @@
   <div>
     <h1>Home</h1>
     <p>Welcome to Tic Tac Toe!</p>
-    <router-link to="/login">Start Game</router-link>
+    <router-link to="/login">Login</router-link>
+    <router-link to="/register">Register</router-link>
   </div>
 </template>
--- Tic Tac Toe/src/views/LoginView.vue
+++ Tic Tac Toe/src/views/LoginView.vue
@@ -1,5 +1,4 @@
 <script>
-console.log("LoginView.vue");
 </script>
 <template>
     <div>
